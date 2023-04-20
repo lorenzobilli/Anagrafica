5588b5f4ed9b00a8e202d28dcce8054eae1bfd32
Lorenzo Billi <lorenzoaebilli@gmail.com>
2023-04-20 17:12:03 +0200

    Added Spring Boot backend stub

---
+++ backend/src/main/java/com/github/lorenzobilli/anagrafica/AnagraficaApplication.java
@@ -0,0 +1,13 @@
+package com.github.lorenzobilli.anagrafica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagraficaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagraficaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/lorenzobilli/anagrafica/AnagraficaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lorenzobilli.anagrafica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagraficaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
